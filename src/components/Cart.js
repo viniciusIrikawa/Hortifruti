7c09ffba22dbd569670f233dbd2b59a126333af4
Vinicius <viniciusiandrade@hotmail.com>
2022-03-05 00:28:09 -0300

    Styles in cart items and basic adjustments

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Context from '../Context';
 import './Cart.css'
 
@@ -6,15 +6,35 @@ const Cart = () => {
   const {cartItems} = useContext(Context);
   console.log(cartItems)
 
+  const [quantity, setQuantity] = useState(1)
+
+  const increment = () => {
+    if(quantity < 10){
+      setQuantity(quantity + 1);
+    }
+  }
+  const decrement = () => {
+    if(quantity > 0){
+      setQuantity(quantity + -1);
+    }
+
+  }
+
   return (
       <section className='view-products'>
           <h1> My cart </h1>
           
           {cartItems.map(item => 
             <div className='itemCart' key={item.id}>
-              <div className='imgItem'>
-                <img src={item.img}/>
-                <h1> Item name: {item.name} </h1>
+              <img className='imgCart' src={item.img}/>
+              <div className='description'>
+                <h3> {item.name} </h3>
+                <h4> {item.genus} </h4>
+                <div className='counter'> 
+                  <button onClick={decrement}> - </button>
+                  <span> {quantity} </span>
+                  <button onClick={increment}> + </button>
+                </div>
               </div>
             </div>
           )}
